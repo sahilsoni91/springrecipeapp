e5d164820ff498defd355c7a3992aad7ae7658c8
Sahil Soni <E01602@hsc.com>
2020-01-15 18:16:04 +0530

    Initial Project Commit

---
+++ src/main/java/demo/app/recipeapp/SpringrecipeappApplication.java
@@ -0,0 +1,13 @@
+package demo.app.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrecipeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrecipeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/app/recipeapp/SpringrecipeappApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.app.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrecipeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
